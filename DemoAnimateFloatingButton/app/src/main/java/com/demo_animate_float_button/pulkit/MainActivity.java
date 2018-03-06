e7aae743a35875e602580411c12eb4ede4445f1f
Pulkit <pulkitaggarwal08@gmail.com>
2018-03-06 20:48:11 +0530

    Initial commit

---
+++ DemoAnimateFloatingButton/app/src/main/java/com/demo_animate_float_button/pulkit/MainActivity.java
@@ -0,0 +1,28 @@
+package com.demo_animate_float_button.pulkit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findIds();
+        init();
+
+    }
+
+    private void findIds() {
+
+    }
+
+    private void init() {
+
+
+    }
+
+
+}
---
+++ DemoAnimateFloatingButton/app/src/main/java/com/demo_animate_float_button/pulkit/floatingpoint/Util.java
@@ -0,0 +1,24 @@
+package com.demo_animate_float_button.pulkit.floatingpoint;
+
+import android.content.Context;
+import android.os.Build;
+
+final class Util {
+
+    private Util() {
+    }
+
+    static int dpToPx(Context context, float dp) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return Math.round(dp * scale);
+    }
+
+    static boolean hasJellyBean() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN;
+    }
+
+    static boolean hasLollipop() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
+    }
+
+}
